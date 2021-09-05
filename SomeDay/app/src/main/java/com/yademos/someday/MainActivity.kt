fb70284a554a08c6ff0847ef0cc3ff03150cc02c
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-09-05 15:11:34 +0900

    [ADD] CalendarView 추가
    Sidemenu 임시 삭제
    CalendarVeiw 추가 및 초기 설정

--- SomeDay/app/src/main/java/com/yademos/someday/MainActivity.kt
+++ SomeDay/app/src/main/java/com/yademos/someday/MainActivity.kt
@@ -1,45 +1,48 @@
 package com.yademos.someday
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
 import android.view.MenuItem
-import android.widget.Toolbar
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import androidx.annotation.NonNull
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var calendarView: MaterialCalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        calendarView = findViewById(R.id.calendarView)
 
+        initToolbar()
+        calendarView.state().edit()
+            .setFirstDayOfWeek(Calendar.SUNDAY)
+            .setMinimumDate(CalendarDay.from(2021, 12, 31))
+            .setMaximumDate(CalendarDay.from(2099, 12, 26))
+            .setCalendarDisplayMode(CalendarMode.MONTHS)
+            .commit();
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_memu)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val result = false
-
-        when(item.itemId) {
-            android.R.id.home -> {
-                val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-                val gravity = GravityCompat.START // 현재 사이드 메뉴의 위치
-
-                if (drawerLayout.isDrawerOpen(gravity)){ // 현재 지금 사이드 메뉴가 열려있는가?
-                    drawerLayout.closeDrawer(gravity)
-                } else {
-                    drawerLayout.openDrawer(gravity)
-                }
-            }
-        }
-        return result
     }
 }
