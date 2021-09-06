84c7bba61aee4e4f8e48e341570a96d9715add84
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-09-06 20:23:26 +0900

    [EDIT] CalendarView 타이틀 변경
    CalendarView의 타이틀을 바꾸었습니다.
    년도를 Appbar에 표시했습니다.

--- SomeDay/app/src/main/java/com/yademos/someday/MainActivity.kt
+++ SomeDay/app/src/main/java/com/yademos/someday/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.yademos.someday
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import androidx.annotation.NonNull
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.CalendarMode
 import java.util.*
+import com.prolificinteractive.materialcalendarview.format.TitleFormatter
+import java.text.SimpleDateFormat
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,23 +18,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val now: Long = System.currentTimeMillis()
+        val date = Date(now)
 
         toolbar = findViewById(R.id.toolbar)
         calendarView = findViewById(R.id.calendarView)
 
         initToolbar()
+
         calendarView.state().edit()
             .setFirstDayOfWeek(Calendar.SUNDAY)
-            .setMinimumDate(CalendarDay.from(2021, 12, 31))
+            .setMinimumDate(CalendarDay.from(2020, 12, 31))
             .setMaximumDate(CalendarDay.from(2099, 12, 26))
             .setCalendarDisplayMode(CalendarMode.MONTHS)
-            .commit();
+            .commit()
+
+        calendarView.setSelectedDate(date)
+
+        setCalendarViewTitle()
+        calendarView.setOnMonthChangedListener { _, _ ->
+            setCalendarViewTitle()
+        }
     }
+
 
     private fun initToolbar() {
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+//        supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_memu)
+    }
+
+    private fun setCalendarViewTitle() {
+        calendarView.setTitleFormatter(TitleFormatter {
+            supportActionBar?.title = it.year.toString()
+            val calendar_view_format = SimpleDateFormat("M월")
+            calendar_view_format.format(it.date.time)
+        })
     }
 }
