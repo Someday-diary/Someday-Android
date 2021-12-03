c0bd71077908c83e63c2ea5237be99e38ee6415e
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-12-03 11:38:40 +0900

    [EDIT] SignUpPwdFragment Add viewModels And Del ViewModelProvider

--- SomeDay/app/src/main/java/com/diary/someday/fragment/SignUpPwdFragment.kt
+++ SomeDay/app/src/main/java/com/diary/someday/fragment/SignUpPwdFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.diary.someday.Enum.ResponseState
@@ -24,7 +24,7 @@ import com.diary.someday.databinding.FragmentSignUpPwdBinding
 
 class SignUpPwdFragment : Fragment() {
     lateinit var binding: FragmentSignUpPwdBinding
-    lateinit var signUpViewModel: SignUpViewModel
+    private val signUpViewModel: SignUpViewModel by viewModels()
     val args: SignUpPwdFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -32,7 +32,7 @@ class SignUpPwdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpPwdBinding.inflate(inflater, container, false)
-        signUpViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+//        signUpViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
 
 
 
